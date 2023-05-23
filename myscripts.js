5cd9983f914711f4fe06fe399f30c95df3953004
Sophia Jawort <sophiejawort@yahoo.com>
2023-05-23 16:06:33 -0700

    drop down menus are functional

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,25 @@
-function myFunction(){
-    console.log();
+function filterSort(option){
+    let curr_selection = document.getElementById('sort-selection');
+    curr_selection.textContent=option;
 }
+
+function filterOffice(option){
+    let curr_selection = document.getElementById('office-selection');
+    curr_selection.textContent=option;
+}
+
+function filterStatus(option){
+    let curr_selection = document.getElementById('status-selection');
+    curr_selection.textContent=option;
+}
+
+function filterJobStat(option){
+    let curr_selection = document.getElementById('job-status-selection');
+    curr_selection.textContent=option;
+}
+
+function filterDept(option){
+    let curr_selection = document.getElementById('dept-selection');
+    curr_selection.textContent=option;
+}
+
