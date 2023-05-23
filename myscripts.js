897f5cc8010bfdf2f3210b379423b48afea64416
Sophia Jawort <sophiejawort@yahoo.com>
2023-05-23 16:23:58 -0700

    added comments , fixed alignment

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,6 @@
+// all these functions have the curr-selection from any dropdown menu saved in the variable curr_selection
+// it then updates the webpage to align with the users selection
+
 function filterSort(option){
     let curr_selection = document.getElementById('sort-selection');
     curr_selection.textContent=option;
@@ -13,6 +16,7 @@ function filterStatus(option){
     curr_selection.textContent=option;
 }
 
+// these two are just for the index.html of job-positions (not relevant to current applicants)
 function filterJobStat(option){
     let curr_selection = document.getElementById('job-status-selection');
     curr_selection.textContent=option;
